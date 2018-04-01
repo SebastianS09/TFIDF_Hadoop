542d15fe20de39fb89e7cc4d96e237b599f35173
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-01 19:16:14 +0200

    Update Pyspark_TFIDF.py
    making a function for looping

--- Pyspark_TFIDF.py
+++ Pyspark_TFIDF.py
@@ -1,32 +1,28 @@
 import math
 import time
 
-t0 = time.time()
+docs = [2,5,10,20,50,100]
+samples = [0.001,0.002,0.005,0.01,0.05,0.1]
 
-text_raw = sc.textFile("/user/hadoop/docgen/input/5000-8.txt")
-
-#Cleaning text and removing punctuation:
-
-text_clean = text_raw.map(lambda x: ''.join([ c for c in x if (c.isalnum() or c==' ')]))
-text_clean = text_clean.filter(lambda x: x != '').map(lambda x: x.lower())
-words = text_clean.flatMap(lambda x: x.split())
-doc_nb = 5
-sample_size = 0.001
-
-documents = []
-documents = [(words.sample(False,sample_size)) for i in range(doc_nb)]
-documents = [sc.parallelize(documents[i].map(lambda x: (i,x)).collect()) for i in range(doc_nb)]
-documents = sc.union(documents)
-doclist = documents.groupByKey().map(lambda x: list(x[1]))
-
-doc_word_count = documents.map(lambda x: ((x[0],x[1]),1)).reduceByKey(lambda x,y: x+y)
-occ_count = doc_word_count.map(lambda x: (x[0][1],1)).reduceByKey(lambda x,y: x+y)
-
-IDF = occ_count.map(lambda x : (x[0],math.log(doc_nb/x[1])))
-#doc_word_count.sortBy(lambda x: x[1],False).collect()
-joined = doc_word_count.map(lambda x: (x[0][1],(x[0][0],x[1]))).join(IDF)
-TFIDF_scores = joined.map(lambda x: (x[0],(x[1][0][0],x[1][0][1]*x[1][1])))
-TFIDF_scores.collect()
-t1 = time.time()
-
-print("elasped time for","doc numb: ",doc_nb,"sample_size: ",sample_size,"time: ",t1-t0)
+def TFIDF(doc,samp):
+    t0 = time.time()
+    text_raw = sc.textFile("/user/hadoop/docgen/input/5000-8.txt")
+    text_clean = text_raw.map(lambda x: ''.join([ c for c in x if (c.isalnum() or c==' ')]))
+    text_clean = text_clean.filter(lambda x: x != '').map(lambda x: x.lower())
+    words = text_clean.flatMap(lambda x: x.split())
+    doc_nb = doc
+    sample_size = samp
+    documents = []
+    documents = [(words.sample(False,sample_size)) for i in range(doc_nb)]
+    documents = [sc.parallelize(documents[i].map(lambda x: (i,x)).collect()) for i in range(doc_nb)]
+    documents = sc.union(documents)
+    doclist = documents.groupByKey().map(lambda x: list(x[1]))
+    doc_word_count = documents.map(lambda x: ((x[0],x[1]),1)).reduceByKey(lambda x,y: x+y)
+    occ_count = doc_word_count.map(lambda x: (x[0][1],1)).reduceByKey(lambda x,y: x+y)
+    IDF = occ_count.map(lambda x : (x[0],math.log(doc_nb/x[1])))
+    joined = doc_word_count.map(lambda x: (x[0][1],(x[0][0],x[1]))).join(IDF)
+    TFIDF_scores = joined.map(lambda x: (x[0],(x[1][0][0],x[1][0][1]*x[1][1])))
+    filename = str(docs)+'_'+str(samples)+'_'"file.csv"
+    TFIDF_scores.saveAsTextFile(filename)
+    t1 = time.time()
+    print("elasped time for","doc numb: ",doc_nb,"sample_size: ",sample_size,"time: ",t1-t0)
