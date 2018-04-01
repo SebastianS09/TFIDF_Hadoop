6bb3f7965678e37989d2002b9b55fd468324827d
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-01 18:37:57 +0200

    Update Pyspark_TFIDF.py

--- Pyspark_TFIDF.py
+++ Pyspark_TFIDF.py
@@ -1,5 +1,5 @@
+import math
 text_raw = sc.textFile("/user/hadoop/docgen/input/5000-8.txt")
-text_raw.take(5)
 
 #Cleaning text and removing punctuation:
 
@@ -14,8 +14,6 @@ documents = [(words.sample(False,sample_size)) for i in range(doc_nb)]
 documents = [sc.parallelize(documents[i].map(lambda x: (i,x)).collect()) for i in range(doc_nb)]
 documents = sc.union(documents)
 doclist = documents.groupByKey().map(lambda x: list(x[1]))
-
-import math
 
 doc_word_count = documents.map(lambda x: ((x[0],x[1]),1)).reduceByKey(lambda x,y: x+y)
 occ_count = doc_word_count.map(lambda x: (x[0][1],1)).reduceByKey(lambda x,y: x+y)
