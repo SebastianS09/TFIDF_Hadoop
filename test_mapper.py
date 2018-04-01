e7b815eda79de5b8e03d058bf831546225f0c393
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-01 10:57:31 +0200

    Update test_mapper.py

--- test_mapper.py
+++ test_mapper.py
@@ -1,7 +1,9 @@
 import sys
+import os 
 
+test = os.environ["TOTALS"]
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     pairs = line.split()
-    print '%s\t%s' % (pairs, 1)
+    print '%s\t%s' % (pairs, test)
