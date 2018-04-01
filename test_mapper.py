db593b01009bc25eb0c2d45988912a0658809f80
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-01 13:20:50 +0200

    Update test_mapper.py

--- test_mapper.py
+++ test_mapper.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 import sys
+file = open('part-00000','r')
+test = file.read()
 
 for line in sys.stdin:
-  print '%s\t%s' % ('lol',1)
+  print '%s\t%s' % (test,1)
 #with open("totals","r") as f:
 #    for line in f:
 #        print line.rstrip()
