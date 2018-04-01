33e674819ba532257cafb00c04a30110e9cb7432
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-01 16:49:17 +0200

    Update IDF_reducer.py
    fixing logic for different doc sizes

--- IDF_reducer.py
+++ IDF_reducer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-
 import sys
+import math
 
 current_word = None
 current_count = 0
@@ -9,9 +9,12 @@ current_occ = 0
 occ_count = 0
 word = None
 
+stats = []
 file = open('part-00000','r')
-doc_numb = file.read()[0]
+stats = file.readline()
 
+stat_dict = dict(stats)
+doc_numb = stat_dict["total docs"]
 
 # input comes from STDIN
 for line in sys.stdin:
@@ -24,6 +27,7 @@ for line in sys.stdin:
     # convert count (currently a string) to int
     try:
         occ = int(occ)
+        count = int(count)
     except ValueError:
         # count was not a number, so silently
         # ignore/discard this line
@@ -31,15 +35,23 @@ for line in sys.stdin:
 
     # this IF-switch only works because Hadoop sorts map output
     # by key (here: word) before it is passed to the reducer
+
     if current_word == word:
         current_occ += 1
+        temp.append((word,ID,count))
     else:
         if current_word:
+            for line in temp:
+              print '%s\t%s\t%s' % (line[0], line[1], line[2]/float(stat_dict[line[1]])*(math.log(float(doc_numb)/current_occ)))
             # write result to STDOUT
-            print '%s\t%s' % (current_word, float(current_occ)/doc_numb)
+            #print '%s\t%s' % (current_word, float(current_occ)/doc_numb)
         current_word = word
         current_occ = 1
+        
+        temp = [(word,ID,count)]
 
 # do not forget to output the last word if needed!
 if current_word == word:
-    print '%s\t%s' % (current_word, float(current_occ)/doc_numb)
+    #print '%s\t%s' % (current_word, float(current_occ)/doc_numb)
+    for line in temp:
+      print '%s\t%s\t%s' % (line[0], line[1], line[2]/float(stat_dict[line[1]])*(math.log(float(doc_numb)/current_occ)))
