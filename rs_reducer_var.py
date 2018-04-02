974c8779e7f4e9b6c961d1911436f9fbd0c91936
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-02 11:42:32 +0200

    Create rs_reducer_var.py

---
+++ rs_reducer_var.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import sys
+import random
+import os
+
+count = 0
+N = 0
+K = os.environ["K"]
+
+key = random.randint(0,10000)
+save = []
+
+# input comes from STDIN
+for line in sys.stdin:
+    item = line.split('\t', 2)[1]
+
+    N += 1 
+    if count < K:
+        save.append(item)
+        count += 1
+    else: 
+        s = random.randint(0,N)
+        if s < K:
+            save[s] = item
+
+for i in save:
+  print '%s\t%s' % (i,key)
