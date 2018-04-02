9adf82bc66efb6e45709573d908eaff9dc140682
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-02 11:37:55 +0200

    Create mapreduce_100.sh

---
+++ testing/mapreduce_100.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -input /user/hadoop/docgen/input -output /user/hadoop/docgen/output -file /home/hadoop/rs_mapper.py -mapper /home/hadoop/rs_mapper.py -file /home/hadoop/rs_reducer.py -reducer /home/hadoop/rs_reducer.py -numReduceTasks 100
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -input /user/hadoop/docgen/output -output /user/hadoop/wc/output -file /home/hadoop/wc_mapper.py -mapper /home/hadoop/wc_mapper.py -file /home/hadoop/wc_reducer.py -reducer /home/hadoop/wc_reducer.py 
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -input /user/hadoop/wc/output -output /user/hadoop/counts/output -file /home/hadoop/total_mapper.py -mapper /home/hadoop/total_mapper.py -file /home/hadoop/total_reducer.py -reducer /home/hadoop/total_reducer.py -numReduceTasks 1
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -input /user/hadoop/wc/output -output /user/hadoop/IDF/output -file /home/hadoop/IDF_mapper.py -mapper /home/hadoop/IDF_mapper.py -file /home/hadoop/IDF_reducer.py -reducer /home/hadoop/IDF_reducer.py -cacheFile /user/hadoop/counts/output/part-00000#part-00000
