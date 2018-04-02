92e518ec29e3474e12bbebd4b6617f6d1e00f8f5
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-02 13:28:09 +0200

    commenting

--- wc_mapper.py
+++ wc_mapper.py
@@ -2,7 +2,12 @@
 
 import sys
 
-#using input already in (word,key) format, but needing to have 1 as key for word count. Thus having the word and the ID concatenated
+#using input already in (word,key) format: here word and document number
+#but we need:
+# 1) to add 1 as key for the word count reducer
+# 2) to have same words from within the same document together: thus concatening word_docnb which will then be sorted
+# example (cat, 4000), (cat,4003), (cat,3000), (cat,4003) will give:
+# (cat_3000,1), (cat_4000,1), (cat_4003,1), (cat_4003,1) thus allowing to properly count by word and document
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
@@ -11,7 +16,7 @@ for line in sys.stdin:
         # write the results to STDOUT (standard output);
         # what we output here will be the input for the
         # Reduce step, i.e. the input for reducer.py
-        # Keeping the ID for future reference
+        # Keeping the ID as mentionned above
         # tab-delimited; the trivial word count is 1
     print '%s\t%s' % (pairs[0]+'_'+pairs[1], 1)
 
