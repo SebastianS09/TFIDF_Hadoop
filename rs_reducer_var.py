14d5e54049bb05f4e0be7aabca000e607ea24632
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-02 13:24:23 +0200

    Commenting

--- rs_reducer_var.py
+++ rs_reducer_var.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
+
 import sys
 import random
 import os
+
+######
+#Special Version of the reducer taking advantage of the -cmdenv option of Hadoop streaming:
+#the parameter K is given actually given in the hadoop mapreduce jar through -cmdenv K=500 for example
+#It is then passed as a new OS environment variable which is accessible by the nodes (here $K) and thus to python using the os library
+#It allows to run testing script much more flexibly by simply passing a parameter
+#######
 
 count = 0
 N = 0
