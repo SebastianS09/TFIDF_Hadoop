333e466320c9d88b23f0cdce8eaece11b350e42e
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-02 13:36:43 +0200

    commenting

--- total_mapper.py
+++ total_mapper.py
@@ -2,11 +2,11 @@
 
 import sys
 
-#counting the total number of documents 
-#counting the total number of words
+#counting the total number of words per document (here it is a fixed size but in case we want to have add variability)
+# input comes from STDIN (standard input), from the word_count mapper in the form 
+# Word, Document, Count
+# We keep only the document and the count to keep a track of the number of words per document
 
-# input comes from STDIN (standard input)
 for line in sys.stdin:
     triplets = line.split()
-
     print '%s\t%s' % (triplets[1],triplets[2])
