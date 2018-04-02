4dd6cbe1dcb85235df551e09acb7a734ba9e9d63
SebastianS09 <30629645+SebastianS09@users.noreply.github.com>
2018-04-02 12:25:24 +0200

    commenting

--- rs_mapper.py
+++ rs_mapper.py
@@ -7,6 +7,8 @@ import re
 subsamp = 0
 
 # input comes from STDIN (standard input)
+#We want to create a list of words
+
 for line in sys.stdin:
     # remove leading and trailing whitespace, punctuation and caps
     line = line.strip()
@@ -16,10 +18,10 @@ for line in sys.stdin:
     # split the line into words
     words = line.split()
 
-    # using the position in the book as key to avoid sorting alphabeltically 
+    # using the position in the book as key to avoid sorting alphabeltically in the shuffle and sort operation
+    # subsampling to take advantage of shuffle and sort algorithm 
     for word in words:
         subsamp += 1
         print '%s\t%s\t%s' % (subsamp, word, 1)
         if subsamp > 10000:
             subsamp = 0
-        
